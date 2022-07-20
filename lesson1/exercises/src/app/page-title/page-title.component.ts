aa71b60d68fe818f54acc7d5996e619aaf598e6d
codeswitchingstl <codeswitching@icloud.com>
2022-07-19 22:48:13 -0500

    some bonus things added

--- lesson1/exercises/src/app/page-title/page-title.component.ts
+++ lesson1/exercises/src/app/page-title/page-title.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-title.component.css']
 })
 export class PageTitleComponent implements OnInit {
-  title = "My Working Title";
+  title = "Possibly The Most Random Things on A Page";
   constructor() { }
 
   ngOnInit() {
